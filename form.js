515c234258970a2d47c0a5f82a5fbf05dbe15201
Hawk2272 <email@gmail.com>
2022-02-24 11:16:26 +0530

    Majors and uphauls to the front and backend

---
+++ form.js
@@ -0,0 +1,22 @@
+function validateForm() {
+  let x = document.forms["form1"]["fname"].value;
+  let y = document.forms["form1"]["lname"].value;
+  let z = document.forms["form1"]["email"].value;
+  
+  if (x =="" && y == "" && z == "") {
+    alert("The form must be filled out");
+    return false;
+  }
+  
+  if (x == "") {
+    alert("First Name must be filled out");
+    return false;
+  } else if (y == "") {
+    alert("Last Name must be filled out");
+    return false;
+  } else if (z == "") {
+    alert("Email must be filled out");
+    return false;
+  }
+  
+}
